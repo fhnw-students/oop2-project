d218ddc24551106076a58d266f77ae38be722ecb
David Heimgartner <david.heimgartner@students.fhnw.ch>
2015-04-21 16:42:44 +0200

    test commit

--- src/main/java/ch/fhnw/oop/AcademyApp.java
+++ src/main/java/ch/fhnw/oop/AcademyApp.java
@@ -3,11 +3,11 @@ package ch.fhnw.oop;
 import javax.swing.*;
 
 public class AcademyApp {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		final CounterModel model = new CounterModel();
 //		final CounterController controller = new CounterController(model);
 //		final CounterView view = new CounterView(model,controller);
 ////		SwingUtilities.invokeLater(controller::initializeView);
 //		SwingUtilities.invokeLater(view::createAndShow);
-	}
+    }
 }
